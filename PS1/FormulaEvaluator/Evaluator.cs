c564b109add708d78f3b9c7e8aa9e52b68e93959
joshjet <joshjet@live.com>
2016-09-05 12:34:20 -0600

    Added comments, and a few helper methods. Got started on the main algorithm by adding the stacks required to execute it.

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FormulaEvaluator
@@ -9,10 +10,37 @@ namespace FormulaEvaluator
     public static class Evaluator
     {
         public delegate int Lookup(String v);
-
+        /// <summary>
+        /// Evaluates integer arthmetic expressions, passed to the method via a string, and a delegate to return the value of any variables found in the string.
+        /// </summary>
+        /// <param name="exp"> An integer expression, represented in the form of a string consisting of parenthesis, the symbols +,-,*,/, 
+        ///                    nonnegative integers, whitespace, and variables consisting of one or more letters followed by one or more digits.</param>
+        /// <param name="variableEvaluator">A delegate that takes variables consisting of one or more letters followed by one or more digits, 
+        ///                    and returns an associated integer value if the variable has one. Throws ArgumentException if the variable has no value. </param>
+        /// <returns>An integer value from the evaluated string if it fits the aformentioned requirements.</returns>
         public static int Evaluate(String exp, Lookup variableEvaluator)
         {
+            //Strip all the whitespace out of the string.
+            Regex.Replace(exp, @"\s", "");
+            //Split the passed string into tokens
+            string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            //Set up stacks and start distributing the tokens into them
+            Stack<string> values = new Stack<string>();
+            Stack<string> operators = new Stack<string>();
+
+
+
             return 0; //TODO: whadayathink
+        }
+        /// <summary>
+        /// Determines whether or not the passed token is a variable. In order to return true, the token must consist of one or more letters followed by one or more numbers.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        private static bool IsVariable(string token)
+        {
+
         }
     }
 }
