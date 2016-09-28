9ec26900f457b443fff4ff02e1be52a8378d256a
joshjet <joshjet@live.com>
2016-09-27 22:18:37 -0600

    Set up test class.

---
+++ PS4Skeleton/UnitTestProject1/SpreadsheetTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SpreadsheetTests
+    {
+        //GetNamesOfAllNonEmptyCells Tests *******************************************************************
+
+        //GetCellContents Exception Tests *******************************************************************
+
+        //GetCellContents Normal Tests *******************************************************************
+
+        //SetCellContents(string, double) Exception Tests *******************************************************************
+
+        //SetCellContents(string, double) Normal Tests *******************************************************************
+
+        //SetCellContents(string, string) Exception Tests *******************************************************************
+
+        //SetCellContents(string, string) Normal Tests *******************************************************************
+
+        //SetCellContents(string, formula) Exception Tests *******************************************************************
+
+        //SetCellContents(string, formula) Normal Tests *******************************************************************
+
+        
+
+
+    }
+}
