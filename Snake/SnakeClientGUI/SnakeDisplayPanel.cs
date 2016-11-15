d70075608ceed932bd421f387bfc1715ca0a8b1e
joshjet <joshjet@live.com>
2016-11-15 12:10:21 -0700

    Further implementation of the model and the View. Focus on the methods required to implement rendering in a simple manner (specific methods to perform transforms, etc.)

--- Snake/SnakeClientGUI/SnakeDisplayPanel.cs
+++ Snake/SnakeClientGUI/SnakeDisplayPanel.cs
@@ -33,5 +33,63 @@ namespace SnakeClientGUI
             e.Graphics.FillRectangle(Brushes.White, background);
             //Insert code here to render the panel.
         }
+
+        /// <summary>
+        /// Draws the snakes for the world, centered on the snake designated as PlayerID 
+        /// </summary>
+        private void drawSnakes(PaintEventArgs e, World world, int PlayerID)
+        {
+            //Look at each snake, to see if its worth drawing
+            foreach (Snake s in world.getLiveSnakes())
+            {
+                foreach (SnakeModel.Point p in s.getVerticies())
+                {
+                    //If any of the snakes verticies are in our view, draw said snake.
+                    if (viewContains(e, world, PlayerID, p))
+                    {
+                        drawSnake(e, world, PlayerID, s.getID());
+                    }
+                    
+                }
+            }
+        }
+        /// <summary>
+        /// Draws the snake specified by snakeID, based upon the view specified by playerID, 
+        /// based upon data from world, in the graphics object specified by PaintEventArgs.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="world"></param>
+        /// <param name="playerID"></param>
+        /// <param name="snakeID"></param>
+        private void drawSnake(PaintEventArgs e, World world, int playerID, int snakeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool viewContains(PaintEventArgs e, World world, int playerID, SnakeModel.Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Takes a global point (referring to some entities absolute position on the map), and transforms it to a relative point, with coordinates 
+        /// relative to the head specified by the passed PlayerID. If the pased PlayerID doesn't refer to a live snake in the passed world, returns the 
+        /// same point, untransformed.
+        /// </summary>
+        /// <param name="globalPoint"></param>
+        /// <returns></returns>
+        private SnakeModel.Point transformGlobalToLocalPoint(World world, int PlayerID, SnakeModel.Point globalPoint)
+        {
+            if (!world.IsPlayerAlive(PlayerID))
+            {
+                return globalPoint;
+            }
+            //Grab the player head location
+            SnakeModel.Point playerHead = world.getHead(PlayerID);
+
+            //A relative point is just a global point, minus the head location of the specified snake. 
+            return new SnakeModel.Point(globalPoint.PointX - playerHead.PointX, globalPoint.PointY - playerHead.PointY);
+        }
     }
 }
